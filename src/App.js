b1cec98bd1fe7c4a2e2a836525069ce2d665ea27
prakashvenigandla <venigandla.prakash@hotmail.com>
2024-03-28 18:01:28 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+          <h1>Hello world, Welcome to React</h1>
+    );
+  }
+}
+
+export default App;
+
+
+
+
+
+
